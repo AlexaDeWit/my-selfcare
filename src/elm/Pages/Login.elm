1a6847f87cffe9246d5e4cd292878171c0641d0d
ChoboBunny <kanin.dewit@gmail.com>
2018-03-24 17:59:28 +0100

    Added a placeholder text entry box

--- src/elm/Pages/Login.elm
+++ src/elm/Pages/Login.elm
@@ -7,9 +7,10 @@ import Style.StyleTags exposing (..)
 import Style.StyleSheet exposing (..)
 import Model exposing (..)
 import Msg exposing (..)
-
+import Element.Input as Input
 
 --The Login Page, for now is just static html, and consumed by the view page if a user model is missing
+
 
 
 login : Html Msg
@@ -17,5 +18,16 @@ login =
     Element.layout stylesheet <|
         column Main
             []
-            [ el NoStyle [] (text "login!")
+            [
+            Input.text NoStyle [ center ] (usernameText)
+            --,Input.text NoStyle [] ("Password")
+            ,button NoStyle [] (text "Login")
             ]
+
+usernameText : Input.Text Styles b Msg
+usernameText =
+    { onChange = (\t -> UpdateUsername t)
+    , value = ""
+    , label = Input.placeholder {text = "Username", label = Input.hiddenLabel "Username"}
+    , options = []
+    }
