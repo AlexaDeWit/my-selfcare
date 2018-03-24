9e9120ede2d95cddb66cd1bc4f9c4b2a9937e850
Alexandra DeWit <alexa.dewit@gmail.com>
2018-03-24 12:10:45 +0100

    Trying to learn to decompose view

--- src/View.elm
+++ src/View.elm
@@ -2,6 +2,7 @@ module View exposing (..)
 
 import Html exposing (Html)
 import Element exposing (..)
+import Element.Attributes exposing (..)
 import Style.StyleTags exposing (..)
 import Style.StyleSheet exposing (..)
 import Model exposing (..)
@@ -16,8 +17,13 @@ view model =
     Element.layout stylesheet <|
         column NoStyle
             []
-            [ el Navigation [] (Maybe.withDefault "Sign Up!" model.username |> text)
+            [ heading model
             , el Header [] (text "Welcome to My SelfCare")
             , el Body [] (text "Here is an example of the body")
             , el Footer [] (text "Here is some Footer stuff")
             ]
+
+
+heading : Model -> Element Styles variation msg
+heading model =
+    el Navigation [ alignRight ] (Maybe.withDefault "Sign Up!" model.username |> text)
